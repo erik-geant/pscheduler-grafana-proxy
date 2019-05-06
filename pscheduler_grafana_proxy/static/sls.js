a8c93a2f200dedf96f4ddc8d9742882c06f92a0b
Erik Reid <erik.reid@geant.org>
2019-05-06 22:09:43 +0200

    filter mp list

--- pscheduler_grafana_proxy/static/sls.js
+++ pscheduler_grafana_proxy/static/sls.js
@@ -3,20 +3,62 @@ var myApp = angular.module('psApp', []);
 myApp.controller('sls', function($scope, $http) {
 
     $scope.mplist = [];
+    $scope.full_mplist = [];
+    $scope.communities = [];
+    $scope.community = "";
+    $scope.tools = [];
+    $scope.tool = "";
     $scope.status = 'not yet loaded';
 
     $http({
         method: 'GET',
-        url: window.location.origin + "/sls/mplist/owping"
+        url: window.location.origin + "/sls/mplist"
     }).then(
         function(rsp) {
-            $scope.mplist = rsp.data;
+            $scope.mplist = rsp.data.map(obj => ({ ...obj, show: true }));
         },
-        function(rsp) {
-            $scope.mplist = ['error'];
-        }
-
-
+        function(rsp) {$scope.mplist = ['error']}
     );
+
+    $http({
+        method: 'GET',
+        url: window.location.origin + "/sls/mptools"
+    }).then(
+        function(rsp) {
+            $scope.tools = rsp.data;
+            $scope.tools.unshift("");  // filter disable option
+        },
+        function(rsp) {$scope.tools = ['error']}
+    );
+
+    $http({
+        method: 'GET',
+        url: window.location.origin + "/sls/mpcommunities"
+    }).then(
+        function(rsp) {
+            $scope.communities = rsp.data;
+            $scope.communities.unshift("");  // filter disable option
+        },
+        function(rsp) {$scope.communities = ['error']}
+    );
+
+    function hide(mp) {
+        if ($scope.tool && !mp.tools.includes($scope.tool)) {
+            return true;
+        }
+        if ($scope.community && !mp.communities.includes($scope.community)) {
+            return true;
+        }
+        return false;
+    }
+
+    $scope.update_filter = function() {
+        $scope.mplist.forEach(
+            function(v, i) {
+                $scope.mplist[i].show = !hide($scope.mplist[i])
+            }
+        )
+    }
+
 
 });
