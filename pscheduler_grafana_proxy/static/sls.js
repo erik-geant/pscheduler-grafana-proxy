94ba1c831dbb59bbcc8998ae8f3a7bb4edaa14e0
Erik Reid <erik.reid@geant.org>
2019-05-07 10:23:56 +0200

    added simple test params

--- pscheduler_grafana_proxy/static/sls.js
+++ pscheduler_grafana_proxy/static/sls.js
@@ -9,8 +9,38 @@ myApp.controller('sls', function($scope, $http) {
     $scope.tools = [];
     $scope.tool = "";
 
+
+    $scope.schedule_repeat_options = ["PT2M", "PT5M", "PT30M", "PT60M"]
+    $scope.schedule_repeat = "PT5M"
+
+    $scope.schedule_slip_options = ["PT2M", "PT5M", "PT10M"]
+    $scope.schedule_slip = "PT5M"
+
+    $scope.schedule_until_options = ["10m", "1h", "1d"]
+    $scope.schedule_until = "1h"
+
     $scope.source = "";
     $scope.destination = "";
+
+    // latency options
+    $scope.packet_count_options = [10, 20, 50, 100]
+    $scope.packet_count = 10
+
+    // TODO: more supported options
+
+    // throughput options
+    $scope.duration_options = ["PT10S", "PT30S", "PT1M"]
+    $scope.duration = "PT30S"
+
+    $scope.interval_options = ["PT5S", "PT10S", "PT30S"]
+    $scope.interval = "PT5S"
+    // test_spec = {
+    //     "schema": 1,
+    //     "source": test_params["source"],
+    //     "dest": test_params["destination"],
+    //     "duration": "PT%sS" % test_params.get("duration", "30"),
+    //     "interval": "PT%sS" % test_params.get("interval", "6")
+    // }
 
     $http({
         method: 'GET',
