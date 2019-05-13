0ef2789406b7aa21b96ee71d71700fc00534e85a
Erik Reid <erik.reid@geant.org>
2019-05-13 20:37:11 +0200

    test with timeseries data

--- test/test_measurement_routes.py
+++ test/test_measurement_routes.py
@@ -1,7 +1,9 @@
 import json
+import os
 
-import responses
+import jsonschema
 import pytest
+import responses
 
 
 @responses.activate
@@ -28,3 +30,62 @@ def test_latency_delays_http(client, measurement_params):
 
     assert rv.status_code == 200
     assert rv.data.decode('utf-8') == 'aabbccdd'
+
+
+def _mock_measurement_responses(dirname):
+
+    with open(os.path.join(dirname, 'test-data.json')) as f:
+        params = json.loads(f.read())
+
+    data = {
+        'https://{mp}/pscheduler/tasks/{task}'.format(**params):
+            'task-info.json',
+        'https://{mp}/pscheduler/tasks/{task}/runs'.format(**params):
+            'runs.json',
+    }
+
+    for run_id in params['runs']:
+        url = 'https://{mp}/pscheduler/tasks/{task}/runs/{run}'.format(
+            run=run_id, **params)
+        data[url] = '{run}.json'.format(run=run_id)
+
+
+    for url, filename in data.items():
+        with open(os.path.join(dirname, filename)) as f:
+            body = f.read()
+        responses.add(responses.GET, url, body=body)
+
+    return params
+
+
+@responses.activate
+def test_latency_timeseries(client):
+    test_data_dirname = os.path.join(
+        os.path.dirname(__file__), 'measurements', 'owamp')
+    params = _mock_measurement_responses(test_data_dirname)
+
+    payload = {
+        'mp': params['mp'],
+        'task': params['task']
+    }
+
+    rv = client.post(
+        '/measurements/timeseries',
+        data=json.dumps(payload),
+        content_type='application/json')
+
+    assert rv.status_code == 200
+
+    timeseries_schema = {
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+        "type": "array",
+        "items": {
+            "type": "array",
+            "items": {"type": "number"},
+            "minItems": 2,
+            "maxItems": 2
+        },
+        "minItems": 1
+    }
+    data = json.loads(rv.data.decode('utf-8'))
+    jsonschema.validate(data, timeseries_schema)
